73baf302ff52b462900faeb9dee9f089aa32a058
terryshi <shidakang@yundianjia.com>
2017-02-04 17:38:45 +0800

    update

--- pyinit.sh
+++ pyinit.sh
@@ -2,6 +2,7 @@
 #make sure pip install scaffold
 if [ $# != 1 ]; then
   echo "please your project name"
+  exit 1
 fi
 pyscaffold -p $1 && cd $1 && touch requirements.txt
 cd $1 && pyvenv .venv && source .venv/bin/activate
